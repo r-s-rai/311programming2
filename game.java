63006a94025d31b3d90ac9d378939f9d892b01d1
r-s-rai <43115888+r-s-rai@users.noreply.github.com>
2020-04-12 08:45:13 -0500

    Question 1 code

--- game.java
+++ game.java
@@ -88,7 +88,7 @@ public class game
 	                for (int vv =0; vv<m; ++vv)
 	                {
 	                    pointtotals[a] = pointtotals[a] + points[0][vv];
-	                    pointtotals[b] = pointtotals[b] + points[0][vv];
+	                    pointtotals[b] = pointtotals[b] + points[1][vv];
 	                }
 	            }
 	        }
